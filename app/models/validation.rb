c466ac42047fb99682118d092a5cc193fa61ce4f
Bunell5090 <Bunell5090.com>
2022-03-16 19:49:00 -0500

    Updated app/models/validation with validaiotn commands

--- app/models/validation.rb
+++ app/models/validation.rb
@@ -1,2 +1,8 @@
 class Validation < ApplicationRecord
+  validates :first_name, presence: true
+  validates :first_name, uniqueness: true
+  validates :last_name, presence: true
+  validates :last_name, uniqueness: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
 end
