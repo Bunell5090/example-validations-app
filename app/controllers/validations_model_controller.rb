3cd54811a1467db174ffc6cf02274445f7f0be1a
Bunell5090 <Bunell5090.com>
2022-03-16 19:45:48 -0500

    Updated Controller file

--- app/controllers/validations_model_controller.rb
+++ app/controllers/validations_model_controller.rb
@@ -1,2 +1,53 @@
 class ValidationsModelController < ApplicationController
+  def index
+    validation = Validations.all
+    render json: Validationss
+  end
+
+  def create
+    validation = Validations.new(
+      first_name: params["first_name"],
+      last_name: params["last_name"], 
+      email: params["email"],
+    )
+
+    if Validations.save #happy path
+      render json: validation
+    else #sad path
+      render json: {error_messages: Validations.errors.full_messages}, status: 422
+    end
+  end
+
+  def show
+    id = params[:id]
+    validation = Validations.find_by(id: id)
+    render json: validation
+  end
+
+  def update
+    id = params[:id]
+    validation = Validations.find_by(id: id)
+
+    Validations.first_name = params[:first_name] || Validations.first_name
+    Validations.last_name = params[:last_name] || Validations.last_name
+    Validations.email = params[:email] || Validations.email
+
+    if Validations.save #happy path
+      render json: validation
+    else #sad path
+      render json: {error_messages: Validations.errors.full_messages}, status: 422
+    end
+  end
+
+  def destroy
+    id = params[:id]
+    validation = Validations.find_by(id: id)
+    Validations.destroy
+
+    render json: {message: "Validations successfully destroyed!"}
+  end
+
+
+
+
 end
