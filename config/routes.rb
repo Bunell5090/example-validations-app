85507b3a5064ef6ea46ac915ef3f04a1b660c686
Bunell5090 <Bunell5090.com>
2022-03-16 19:40:03 -0500

    Updated Routes file & cors file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/validations" => "validations#index"
+  post "/validations" => "validations#create"
+  get "/validations/:id" => "validations#show" 
+  patch "/validations/:id" => "validations#update"
+  delete "/validations/:id" => "validations#destroy"
 end
